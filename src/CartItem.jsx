412a26d9b0a736ce6b379417673924e85ba9c2d2
dishant <dishantgandhi99@yahoo.com>
2020-06-08 13:04:45 +0530

    added increaseQuantity

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,17 +1,46 @@
 import React, { Component } from "react";
 class CartItem extends Component {
-  state = {};
+  constructor() {
+    super();
+    this.state = {
+      price: 999,
+      title: "Mobile Phone",
+      qty: 1,
+      img: "",
+    };
+  }
+  increaseQuantity = () => {
+    console.log(this);
+  };
   render() {
+    const { price, title, qty } = this.state;
     return (
       <div className="cart-item">
         <div className="left-block">
           <img style={styles.image}></img>
         </div>
         <div className="right-block">
-          <div>Phone</div>
-          <div>Rs 999</div>
-          <div>Qty: 1</div>
-          <div className="cart-items-actions"></div>
+          <div>{title}</div>
+          <div>{price}</div>
+          <div>{qty}</div>
+          <div className="cart-items-actions" title="Icons">
+            <img
+              className="action-icons"
+              src="https://image.flaticon.com/icons/svg/992/992651.svg"
+              alt="Increase"
+              onClick={this.increaseQuantity}
+            />
+            <img
+              className="action-icons"
+              src="https://image.flaticon.com/icons/svg/864/864373.svg"
+              alt="Decrease"
+            />
+            <img
+              className="action-icons"
+              src="https://image.flaticon.com/icons/svg/1345/1345874.svg"
+              alt="Delete"
+            />
+          </div>
         </div>
       </div>
     );
